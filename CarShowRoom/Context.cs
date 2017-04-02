408aada4fa69255ddd20a2f72898037a5d7a3b0e
vasterlord <vasterlord619@gmail.com>
2017-04-02 21:19:08 +0300

    Add entity CarModel and his context

---
+++ CarShowRoom/Context.cs
@@ -0,0 +1,15 @@
+﻿using CarShowRoom.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarShowRoom
+{
+    public class Context : DbContext
+    { 
+        public DbSet<CarModel> CarModels { get; set; }
+    }
+}
